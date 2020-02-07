5c4362c7f9e37252d7ecd52ebca0c58b7e7cec68
ellreka <ellreka5364@gmail.com>
2020-02-07 23:35:42 +0900

    first commit

---
+++ src/index.ts
@@ -0,0 +1,80 @@
+import got from 'got';
+import yargs from 'yargs';
+
+const argv = yargs
+  .usage('$0 <cmd> [args]')
+  .option('slack-token', {
+    describe: "slack's token",
+    type: 'string',
+  })
+  .option('slack-channel', {
+    demandOption: true,
+    describe: 'to post slack channel',
+    type: 'string',
+  })
+  .option('backlog-api-key', {
+    describe: "backlog's api key",
+    type: 'string',
+  })
+  .option('backlog-space', {
+    describe: "backlog's space name",
+    type: 'string',
+  })
+  .option('backlog-user-id', {
+    describe: "backlog's userid",
+    type: 'string',
+  })
+  .help().argv;
+const {
+  slackToken,
+  slackChannel,
+  backlogSpace,
+  backlogApiKey,
+  backlogUserId,
+} = argv;
+console.log(
+  slackToken,
+  slackChannel,
+  backlogSpace,
+  backlogApiKey,
+  backlogUserId,
+);
+(async () => {
+  try {
+    const response = await got(
+      `https://${backlogSpace}.backlog.jp/api/v2/issues?apiKey=${backlogApiKey}&assigneeId[]=${backlogUserId}&startDateSince=2020-02-07&startDateUntil=2020-02-07&sort=priority`,
+    );
+    const issuesData = JSON.parse(response.body).map(
+      (val: {
+        issueKey: string;
+        summary: string;
+        priority: {
+          id: number;
+          name: string;
+        };
+      }) => {
+        return {
+          title: val.issueKey,
+          title_link: `https://geek.backlog.jp/view/${val.issueKey}`,
+          text: `${val.summary}\n優先度：${val.priority.name}\n予定時間：1h`,
+        };
+      },
+    );
+    console.log(issuesData);
+    const params = {
+      token: slackToken,
+      channel: slackChannel,
+      attachments: issuesData,
+    };
+
+    await got.post('https://slack.com/api/chat.postMessage', {
+      headers: {
+        'content-type': 'application/json',
+        Authorization: `Bearer ${slackToken}`,
+      },
+      json: params,
+    });
+  } catch (error) {
+    console.log(error.response.body);
+  }
+})();
